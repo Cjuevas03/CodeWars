3a5caa41ce1ad165f9c3be2553b64529ac0db342
lTokko <jhomarcuevas03@gmail.com>
2021-10-01 19:18:02 -0400

    Multiples35

--- codewars.js
+++ codewars.js
@@ -127,4 +127,15 @@ function generateShape(integer){
     return str
   }
 
+  // Multiples of 3 or 5
+  function solution(number){
+    let sum = 0;
+    
+    for (let i = 1; i < number; i++){
+      if (i % 3 === 0) sum += i;
+      else if (i % 5 === 0) sum += i;
+    }
+    return sum;
+  }
+
   
