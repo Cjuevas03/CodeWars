254dfd7cc520851dcba5becc89702d3ac000e305
lTokko <jhomarcuevas03@gmail.com>
2021-10-05 19:19:56 -0400

    Word Mixer

--- codewars.js
+++ codewars.js
@@ -146,4 +146,18 @@ function generateShape(integer){
    
    };
 
-  
+   // word mixer.
+   function findOdd(A) {
+    const map = new Map();
+    
+    for (let i = 0; i <A.length; i++){
+      if (!map.has(A[i])) map.set(A[i], 1);
+      else {
+        map.set(A[i], map.get(A[i]) + 1);
+      }
+    }
+      for (let [key, value] of map){
+        if (value % 2 !== 0) return key;
+      }
+    return 0;
+  }
