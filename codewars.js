dc451ed11dd563238dd62930bcea3387d5766166
lTokko <jhomarcuevas03@gmail.com>
2021-10-06 22:35:29 -0400

    Phone Numbers

--- codewars.js
+++ codewars.js
@@ -161,3 +161,12 @@ function generateShape(integer){
       }
     return 0;
   }
+
+
+// Create Phone Number
+function createPhoneNumber(numbers){
+  return "(" + numbers[0] + numbers[1] + numbers[2] + ") " +
+      numbers[3] + numbers[4] + numbers[5] + "-" +
+      numbers[6] + numbers[7] + numbers[8] + numbers[9];
+    
+}
