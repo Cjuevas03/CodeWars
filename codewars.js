1a51d2f558d3c42a3f6292d2e12b38cdd3bc67c4
lTokko <jhomarcuevas03@gmail.com>
2021-10-09 22:39:34 -0400

    HighLow

--- codewars.js
+++ codewars.js
@@ -192,3 +192,20 @@ function findShort(s){
   
   return shortest
 }
+//Highest and Lowest
+function highAndLow(numbers){
+  num=numbers.split(' ');
+  var max = parseInt(num[0]);
+  var min = parseInt(num[0]);
+  for (var i = 0; i <= num.length; i++) {
+    if(parseInt(num[i]) > max){
+      max = parseInt(num[i]);
+    }
+  }
+  for (var i = 0; i <= num.length; i++) {
+    if(parseInt(num[i]) < min){
+      min = parseInt(num[i]);
+    } 
+ }
+ return (max + " " + min);
+}
