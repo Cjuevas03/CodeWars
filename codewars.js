fe333e3b41db47c7c3cbaba6212fc2ef52b68d34
lTokko <jhomarcuevas03@gmail.com>
2021-10-15 11:03:20 -0400

    ConverttoBinary

--- codewars.js
+++ codewars.js
@@ -299,5 +299,8 @@ function toCamelCase(str){
   return str.split(/[,_-]+/).map( (s, i) => i !== 0 ? s.charAt(0).toUpperCase() + s.slice(1) : s).join('');
 }
 
-
+/// Convert to Binary
+function toBinary(n){
+  return +n.toString(2);
+}
 
