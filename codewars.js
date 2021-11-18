e5db00292042fd32db770dc3fd3417fdf93bc412
lTokko <jhomarcuevas03@gmail.com>
2021-11-18 05:31:28 -0500

    sumwithouthighestadnlowest

--- codewars.js
+++ codewars.js
@@ -460,4 +460,15 @@ return n
 }
 }
 
-//
+//sumWithoutHighestAndLowestNumber
+
+function sumArray(array) {
+  if (array === null || array.length <= 2){
+    return 0
+  } else if (array.length === 3){
+    array.sort((a,b) => a -b)
+    return array[1]
+  }else {
+    return array.sort((a,b) => a - b).slice(1, array.length - 1).reduce( (a,b) => a + b)
+  }
+}
