78f1d1ec813ef9049fb267e2facf9fdb1a13c7b1
lTokko <jhomarcuevas03@gmail.com>
2022-01-12 13:49:09 -0500

    billboard

--- codewars.js
+++ codewars.js
@@ -763,4 +763,11 @@ function readOut(acrostic) {
 }
 
 
-//
+//www.codewars.com/kata/570e8ec4127ad143660001fd/solutions/javascript
+
+
+function billboard(name, price = 30){
+  let cost = 0
+  for (let i = 1; i <= name.length; i++){cost += price}
+  return cost
+} 
