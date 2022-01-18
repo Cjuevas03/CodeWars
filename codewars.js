3caecca0acc45181b641cd8c2bfea18844d207d3
lTokko <jhomarcuevas03@gmail.com>
2022-01-18 14:03:39 -0500

    inArray

--- codewars.js
+++ codewars.js
@@ -820,3 +820,21 @@ function likes(names) {
   }
 }
 
+//www.codewars.com/kata/550554fd08b86f84fe000a58/train/javascript
+function inArray(array1,array2){
+  return array1.filter(sub => array2.some(x => x.includes(sub))
+    ).sort()
+}
+
+//Alternative Solutions:
+function inArray(a1, a2) {
+  let str = a2.join(' ');
+  return a1.filter(s => str.indexOf(s) !== -1).sort();
+}
+
+function inArray(array1,array2){
+  return array1
+    .filter(a1 => array2.find(a2 => a2.match(a1)))
+    .sort()
+}
+
