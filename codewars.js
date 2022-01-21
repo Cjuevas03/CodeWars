b16c6930b95a4b62a285345ba21c8745732ced47
lTokko <jhomarcuevas03@gmail.com>
2022-01-21 14:09:39 -0500

    takeawalk

--- codewars.js
+++ codewars.js
@@ -845,3 +845,14 @@ function tribonacci(signature,n){
   return signature.slice(0, n)
 }
 
+
+//www.codewars.com/kata/54da539698b8a2ad76000228/train/javascript
+
+function isValidWalk(walk) {
+  const north = walk.filter(x => x === "n").length;
+  const south = walk.filter(x => x === "s").length;
+  const east = walk.filter(x => x === "e").length;
+  const west = walk.filter(x => x === "w").length;
+  
+  return walk.length === 10 && north === south && east === west;
+}
